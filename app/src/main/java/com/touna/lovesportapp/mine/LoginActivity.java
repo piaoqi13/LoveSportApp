cd4f63dc2d2c449137c5a3b0ff1b85509990ab69
piaoqi13@qq.com <piaoqi13@qq.com>
2015-12-11 17:14:14 +0800

    登录UI逻辑完成

--- app/src/main/java/com/touna/lovesportapp/mine/LoginActivity.java
+++ app/src/main/java/com/touna/lovesportapp/mine/LoginActivity.java
@@ -1,10 +1,68 @@
 package com.touna.lovesportapp.mine;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.touna.lovesportapp.BaseActivity;
+import com.touna.lovesportapp.R;
+import com.touna.lovesportapp.view.ClearEditText;
+import com.touna.lovesportapp.view.TitleView;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 
 /**
  * created by collin on 2015-12-09.
  */
-public class LoginActivity extends BaseActivity {
+@ContentView(R.layout.activity_login)
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    @ViewInject(R.id.tv_common_title)
+    private TitleView mTitleView;
 
+    @ViewInject(R.id.cedt_login_phone)
+    private ClearEditText mCedtPhone;
+
+    @ViewInject(R.id.tv_login_forget_password)
+    private TextView mTvForgetPassword;
+
+    @ViewInject(R.id.cedt_login_password)
+    private ClearEditText mCedtPassword;
+
+    @ViewInject(R.id.btn_login)
+    private Button mBtnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mTitleView.setTitle(R.string.title_login);
+        mTitleView.setLeftNavigationImg(R.drawable.back_normal, this);
+        mTitleView.setRightNavigationText(R.string.title_register, this);
+    }
+
+    @Event(R.id.tv_login_forget_password)
+    private void forgetPasswordEvent(View view) {
+        Log.i("CollinWang", "forgetPasswordEvent is run");
+    }
+
+    @Event(R.id.btn_login)
+    private void loginEvent(View view) {
+        Log.i("CollinWang", "loginEvent is run");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_left_navigation:
+                Log.i("CollinWang", "ll_left_navigation is run");
+                break;
+            case R.id.ll_right_navigation:
+                Log.i("CollinWang", "ll_right_navigation is run");
+                break;
+        }
+    }
 }
